b4d6fb7014e3d75e0521c269e5b154e89772befe
JMicahMan <j.micah.manzanares@gmail.com>
2019-05-24 08:31:34 -0500

    I think I got the Evade right

--- Assets/Scripts/EvadeBehaviour.cs
+++ Assets/Scripts/EvadeBehaviour.cs
@@ -6,13 +6,27 @@ public class EvadeBehaviour : MonoBehaviour
 
 {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject Goal;
+    public float Speed;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+        Vector3 Velocity = (Goal.GetComponent<PursuitBehaviour2>().Speed * Goal.transform.forward);
+        Vector3 Direction = Goal.transform.position - transform.position;
+        transform.forward = Velocity.normalized - Direction.normalized;
+        transform.position += transform.forward * (Time.deltaTime * Speed * Vector3.Distance(transform.position, Goal.transform.position));
+
+    }
 }
